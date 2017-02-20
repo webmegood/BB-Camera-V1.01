afe82b1fb0ae02d0929c6115a7863a7e8c8f50d4
webmegood <webmegood@gmail.com>
2017-02-20 14:03:02 +1100

    Add files via upload

--- js/image-capture2.js
+++ js/image-capture2.js
@@ -24,3 +24,36 @@ function onFail(message) {
 }
 
 
+
+function movePic(file){ 
+    window.resolveLocalFileSystemURI(file, resolveOnSuccess, resOnError); 
+} 
+
+//Callback function when the file system uri has been resolved
+function resolveOnSuccess(entry){ 
+    var d = new Date();
+    var n = d.getTime();
+    //new file name
+    var newFileName = n + ".jpg";
+    var myFolderApp = "http://www.mediathrong.com/beepboards/tracking/bbtracker1.10/repo";
+
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSys) {      
+    //The folder is created if doesn't exist
+    fileSys.root.getDirectory( myFolderApp,
+                    {create:true, exclusive: false},
+                    function(directory) {
+                        entry.moveTo(directory, newFileName,  successMove, resOnError);
+                    },
+                    resOnError);
+                    },
+    resOnError);
+}
+
+//Callback function when the file has been moved successfully - inserting the complete path
+function successMove(entry) {
+    alert("Photo successfully uploaded");
+}
+
+function resOnError(error) {
+    alert(error.code);
+}
