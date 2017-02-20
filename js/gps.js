6d3080797b3cc8bdd0f69efbc9a10434043ff9f1
webmegood <webmegood@gmail.com>
2017-02-20 11:33:26 +1100

    Add files via upload

---
+++ js/gps.js
@@ -0,0 +1,205 @@
+
+
+document.addEventListener('deviceready', setupGeolocation, false);
+
+
+
+function setupGeolocation() {
+	
+		
+		// Remove buttons
+		var uploadElement =  document.getElementById('uploadBtn');
+		if (typeof(uploadElement) != 'undefined' && uploadElement != null)
+		{
+			uploadElement.parentNode.removeChild(uploadElement);
+		}		
+		var restartElement = document.getElementById('restartBtn');
+		if (typeof(restartElement) != 'undefined' && restartElement != null)
+		{
+			restartElement.parentNode.removeChild(restartElement);
+		}		
+				
+		// Create stop button
+		var uploadDataButton = document.createElement("div");
+		uploadDataButton.innerHTML = "Stop Tracking";
+		
+		// Append in button zone and append classes and id
+		var positionUploadBtn = document.getElementById('button_zone');
+		positionUploadBtn.appendChild(uploadDataButton);
+		uploadDataButton.className = "button button-primary";
+		uploadDataButton.setAttribute("id", "uploadBtn");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+
+    /**
+    * This callback will be executed every time a geolocation is recorded in the background.
+    */
+	
+	var testObject = [];	 // Array containing GPS location objects	 
+
+	var callbackFn = function(location) {
+
+
+		testObject.push([location.latitude, location.longitude, location.time]);
+
+
+
+		// Store
+		if(typeof(window.localStorage) != 'undefined'){ 
+		//var gpsDataArray = JSON.stringify(testObject);
+		var gpsDataArray = JSON.stringify(testObject);
+		localStorage.setItem('testObject', gpsDataArray);
+		//localStorage.setItem('testObject', JSON.stringify(testObject));
+		
+		//send to database once meeting conditions. (if connected, try straight away then wait 60 seconds.)
+		
+		
+		
+		// Add event handler to button
+		uploadDataButton.addEventListener ("click", function() {
+			backgroundGeolocation.stop(); //this will stop the tracking
+		  	sendtodatabase(gpsDataArray);
+		});
+
+
+		
+		} else {
+		alert("GPS Data Not Available"); 
+		}
+		
+		
+		// Retrieve 
+		var retrievedObject = localStorage.getItem('testObject');
+		document.getElementById("result").innerHTML = retrievedObject;	
+
+
+       // backgroundGeolocation.finish();
+		
+    };
+	
+	
+	
+	
+
+    var failureFn = function(error) {
+        console.log('BackgroundGeolocation error');
+    };
+	
+	
+	
+	
+	
+
+    // BackgroundGeolocation is highly configurable. See platform specific configuration options
+    backgroundGeolocation.configure(callbackFn, failureFn, {
+        desiredAccuracy: 10,
+        stationaryRadius: 10,
+        distanceFilter: 10,
+    	interval: 30000, // <!-- poll for position every 30 secs 
+		//locationService: backgroundGeoLocation.service.ANDROID_FUSED_LOCATION,
+        debug: false, // <-- enable this hear sounds for background-geolocation life-cycle.
+        stopOnTerminate: true // <-- enable this to clear background location settings when the app terminates
+    });
+
+    // Turn ON the background-geolocation system.
+    backgroundGeolocation.start();
+
+    // If you wish to turn OFF background-tracking, call the #stop method.
+    // backgroundGeolocation.stop();
+	
+	
+	
+	
+	
+	
+	
+	
+function sendtodatabase(arrayValues){
+
+$.ajax({
+url: 'http://www.mediathrong.com/beepboards/tracking/scripts/gps_check.php',
+type: 'POST',
+data: {data: arrayValues},
+cache: false,
+success: function(output){
+dit = output;
+},
+error: function (request, status, error) {
+}
+});
+
+//alert("Data has been sent!");
+
+
+
+createRestartButton();
+
+
+}
+
+
+
+
+
+
+
+
+
+function createRestartButton() {	
+
+		// Stop Spinner
+		$("#spinner").fadeOut(100);
+		
+		// Remove Upload button
+		var uploadElement =  document.getElementById('uploadBtn');
+		uploadElement.parentNode.removeChild(uploadElement);
+		
+		// Create Restart button
+		var restartButton = document.createElement("div");
+		restartButton.innerHTML = "Restart Tracking";
+
+		// Append Restart button in main content area and append classes and id
+		var positionRestartBtn = document.getElementById('button_zone');
+		positionRestartBtn.appendChild(restartButton);
+		restartButton.className = "button button-primary";
+		restartButton.setAttribute("id", "restartBtn");
+
+		// Add event handler
+		restartButton.addEventListener ("click", function() {
+				// Start Spinner
+				$("#spinner").fadeIn(100);
+		  	setupGeolocation();
+		});
+		
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
---
+++ js/image-capture.js
@@ -0,0 +1,74 @@
+$("#takePicBtn").click(function(){
+      capturePhoto();
+});
+
+
+
+
+function capturePhoto() {
+	
+
+//alert("noooo!");
+
+
+
+var pictureSource;   // picture source
+var destinationType; // sets the format of returned value
+ 
+document.addEventListener("deviceready", onDeviceReady, false);
+ 
+function onDeviceReady() {
+    pictureSource = navigator.camera.PictureSourceType;
+    destinationType = navigator.camera.DestinationType;
+}
+ 
+function clearCache() {
+    navigator.camera.cleanup();
+}
+ 
+var retries = 0;
+function onCapturePhoto(fileURI) {
+    var win = function (r) {
+        clearCache();
+        retries = 0;
+        alert('Done!');
+    }
+ 
+    var fail = function (error) {
+        if (retries == 0) {
+            retries ++
+            setTimeout(function() {
+                onCapturePhoto(fileURI)
+            }, 1000)
+        } else {
+            retries = 0;
+            clearCache();
+            alert('Ups. Something wrong happens!');
+        }
+    }
+ 
+    var options = new FileUploadOptions();
+    options.fileKey = "file";
+    options.fileName = fileURI.substr(fileURI.lastIndexOf('/') + 1);
+    options.mimeType = "image/jpeg";
+    options.params = {}; // if we need to send parameters to the server request
+    var ft = new FileTransfer();
+    ft.upload(fileURI, encodeURI("http://www.mediathrong.com/beepboards/tracking/bbtracker1.10/repo"), win, fail, options);
+}
+ 
+function capturePhoto() {
+    navigator.camera.getPicture(onCapturePhoto, onFail, {
+        quality: 100,
+        destinationType: destinationType.FILE_URI
+    });
+}
+ 
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
+
+
+
+
+}
